da10667fe217dcf66cba8e8e1b2fe20be8b3f7fc
VPbanna123 <vijaypalsinghrathore8959179600@gmail.com>
2026-02-06 10:46:30 +0530

    site url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,27 @@
+// /** @type {import('next-sitemap').IConfig} */
+// module.exports = {
+//   siteUrl: process.env.SITE_URL ||
+//     (process.env.NEXT_PUBLIC_VERCEL_URL
+//       ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+//       : 'https://example.com'),
+//   generateRobotsTxt: true,
+//   generateIndexSitemap: false,
+//   robotsTxtOptions: {
+//     policies: [{ userAgent: '*', allow: '/' }],
+//   },
+// };
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 
-    (process.env.NEXT_PUBLIC_VERCEL_URL 
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-      : 'https://example.com'),
+  siteUrl: `'https://vijaypal.me'`,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
   },
 };
